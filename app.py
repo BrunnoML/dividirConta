b9dc4bad03e15dc79decd619bb11fc779869507c
BRUNNO <brunnoml@gmail.com>
2024-05-25 15:08:41 -0300

    env and add app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Título e Descrição do APP
+st.title("Divisão de Conta com Gorjeta")
+st.write("Divida sua conta de maneira rápida e fácil!")
+
+# Obtendo os Valores do Usuário com Sliders e Seleção:
